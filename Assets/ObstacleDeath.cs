e607bd875c058c405347042501732c7a168ec721
Connor Ryan <c16755215@mydit.ie>
2019-02-25 15:57:53 +0000

    respawn

---
+++ Assets/ObstacleDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDeath : MonoBehaviour {
+
+    /*private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }*/
+
+    [SerializeField]private Transform player;
+    [SerializeField]private Transform respawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
