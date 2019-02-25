c7570d43c61c78b089a78361d727981a6a27d47a
Mark <c16765279@mydit.ie>
2019-02-25 17:00:54 +0000

    fixed double height jump when standing beside another player

--- Assets/GroundCheck.cs
+++ Assets/GroundCheck.cs
@@ -7,16 +7,30 @@ public class GroundCheck : MonoBehaviour
     public MovementScript ms;
 
     public MovementScriptPlayer2 msSwing;
+
+    public float jumpCoolDown;
+    private float jumpCoolDownInitial;
+
+    public bool jumping;
     // Start is called before the first frame update
     void Start()
     {
+        jumpCoolDownInitial = jumpCoolDown;
         ms = GetComponentInParent<MovementScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (jumping)
+        {
+            jumpCoolDown += Time.deltaTime;
+        }
+
+        if (jumpCoolDown >= jumpCoolDownInitial)
+        {
+            jumping = false;
+        }
     }
     
     private void OnTriggerStay2D(Collider2D other)
@@ -24,10 +38,12 @@ public class GroundCheck : MonoBehaviour
         if (other.gameObject.CompareTag("ground")||other.gameObject.CompareTag("Player"))
         {
             ms.rb.velocity = new Vector3(ms.x * ms.speed, ms.rb.velocity.y, 0);
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space)&&!jumping)
             {
 
                 ms.Jumping();
+                jumping = true;
+                jumpCoolDown = 0;
 
             } 
             
--- Assets/GroundCheck2.cs
+++ Assets/GroundCheck2.cs
@@ -7,16 +7,29 @@ public class GroundCheck2 : MonoBehaviour
     public MovementScriptPlayer2 ms;
 
     public MovementScript msSwing;
+    public float jumpCoolDown;
+    private float jumpCoolDownInitial;
+
+    public bool jumping;
     // Start is called before the first frame update
     void Start()
     {
+        jumpCoolDownInitial = jumpCoolDown;
         ms = GetComponentInParent<MovementScriptPlayer2>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (jumping)
+        {
+            jumpCoolDown += Time.deltaTime;
+        }
+
+        if (jumpCoolDown >= jumpCoolDownInitial)
+        {
+            jumping = false;
+        }
     }
     
     private void OnTriggerStay2D(Collider2D other)
@@ -24,10 +37,12 @@ public class GroundCheck2 : MonoBehaviour
         if (other.gameObject.CompareTag("ground")||other.gameObject.CompareTag("Player"))
         {
             ms.rb.velocity = new Vector3(ms.x * ms.speed, ms.rb.velocity.y, 0);
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (Input.GetKeyDown(KeyCode.UpArrow) && !jumping)
             {
 
                 ms.Jumping();
+                jumping = true;
+                jumpCoolDown = 0;
 
             } 
             
