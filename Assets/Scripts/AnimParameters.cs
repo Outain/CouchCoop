151b77f81e64698d4c28b156ddc523aed5319660
ConorFT <43806423+ConorFT@users.noreply.github.com>
2019-02-25 00:38:20 +0000

    Character and Basic Enviornment
    Character Sprite, with some animations (not properly rigged yet)
    Wall and Floor sprites

---
+++ Assets/Scripts/AnimParameters.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimParameters : MonoBehaviour
+{
+
+    public KeyCode move = KeyCode.A;
+    public Animator anim;
+
+    bool moving;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        moving = false;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(move))
+        {
+            moving = true;
+        } else
+        {
+            moving = false;
+        }
+
+        anim.SetBool("Move", moving);
+    }
+
+
+}
