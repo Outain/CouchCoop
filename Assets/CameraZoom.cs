84f36025dc291f13ff72fd3be67ffbe92daf1a08
Connor Ryan <c16755215@mydit.ie>
2019-02-25 14:28:02 +0000

    camera zoom

---
+++ Assets/CameraZoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    int zoom = 20;
+    int normal = 5;
+    float smooth = 5;
+
+    private bool isZoomed = false;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            isZoomed = !isZoomed;
+        }
+
+        if(isZoomed)
+        {
+            GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, zoom, Time.deltaTime * smooth);
+        }
+
+        else
+        {
+            GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, normal, Time.deltaTime * smooth);
+        }
+    }
+}
