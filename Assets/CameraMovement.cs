1a38aa0b07d9e3de8ec36ec09d685c31619858fd
Mark <c16765279@mydit.ie>
2019-02-27 15:55:44 +0000

    updated camera script

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public Transform player1, player2;
+
+    public Vector3 halfWayPoint;
+
+    private Vector3 cameraPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,8 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        halfWayPoint = player1.position + (player2.position - player1.position) / 2;
+        cameraPos = new Vector3(halfWayPoint.x,halfWayPoint.y,-10);
+        transform.position = cameraPos;
     }
 }
