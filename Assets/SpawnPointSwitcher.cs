df21b2f2e179d22d299f15d6c5b9fe9beb067f6b
Mark <c16765279@mydit.ie>
2019-02-28 22:34:43 +0000

    stitching scenes

---
+++ Assets/SpawnPointSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointSwitcher : MonoBehaviour
+{
+    public GameObject spawnPoint;
+
+    public Transform spawnPoint1, spawnPoint2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint.transform.position = spawnPoint1.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            spawnPoint.transform.position = spawnPoint2.position;
+        }
+    }
+}
