04cd2d1b1daf336d05996ab94a90ea4c9fb93a61
Mark <c16765279@mydit.ie>
2019-02-28 13:17:16 +0000

    new switch option

--- Assets/OpenDoor.cs
+++ Assets/OpenDoor.cs
@@ -6,16 +6,22 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     public GameObject door;
-    private bool _pressed;
+    public bool _pressed;
     public Transform openDoorPos;
     public float doorOpenSpeed, switchOpenSpeed;
     public bool destroyObject;
     public GameObject objectToDestroy;
     
-    private Vector2 _pressedSwitchTransform;
+    
+    private Vector2 _pressedSwitchTransform,_originalSwitchTransform;
+
+    public bool linkedSwitch;
+
+    public GameObject switchToLink;
     // Start is called before the first frame update
     void Start()
     {
+        _originalSwitchTransform = transform.position;
         _pressed = false;
         _pressedSwitchTransform = new Vector2(transform.position.x, transform.position.y-0.2f);
     }
@@ -26,7 +32,7 @@ public class OpenDoor : MonoBehaviour
         if (_pressed)
         {
             transform.position = Vector2.Lerp(transform.position, _pressedSwitchTransform, switchOpenSpeed*Time.deltaTime);
-            door.transform.position = Vector2.Lerp(door.transform.position, openDoorPos.position, doorOpenSpeed*Time.deltaTime);
+            door.transform.position = Vector2.MoveTowards(door.transform.position, openDoorPos.position, doorOpenSpeed*Time.deltaTime);
             if (destroyObject)
             {
                 if (objectToDestroy != null)
@@ -34,6 +40,13 @@ public class OpenDoor : MonoBehaviour
                     Destroy(objectToDestroy);
                 }
             }
+
+           
+        }
+
+        if (!_pressed)
+        {
+            transform.position = Vector2.Lerp(transform.position, _originalSwitchTransform, switchOpenSpeed*Time.deltaTime);
         }
     }
 
@@ -42,7 +55,19 @@ public class OpenDoor : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             _pressed = true;
+            if (linkedSwitch)
+            {
+                if (switchToLink != null)
+                {
+                    switchToLink.GetComponent<OpenDoor>()._pressed = false;
+                }
+            }
         }
+    }
+
+    public void Pressed()
+    {
+        
     }
     
    
